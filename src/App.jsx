02f5e709ee48222a982770b2b8d2f562f0b75773
Orsouene <orsoueneelaouizeb@gmail.com>
2024-12-13 13:46:36 +0100

    Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // IMPORTO HEADER
 import Header from "./assets/Components/Header";
-
+// IMPORTO IL FOOTER
+import Footer from "./assets/Components/Footer";
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/assets/Components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer>
+      <p className="footer-p">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est molestias
+        pariatur sunt. Quibusdam amet corporis repellendus error laudantium quam
+        excepturi. Quibusdam amet corporis repellendus error laudantium quam
+        excepturi.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
